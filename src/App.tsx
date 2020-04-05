f2a323f56a513456eb4686401b5727756753af80
Turbo <turbothinh@gmail.com>
2020-04-05 16:16:39 +0200

    Add Ionic react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { IonButton, IonDatetime } from '@ionic/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<IonDatetime displayFormat="MM/DD/YYYY" placeholder="Select Date"></IonDatetime>
+			<IonButton fill="clear">Start</IonButton>
+		</div>
+	);
 }
 
 export default App;
